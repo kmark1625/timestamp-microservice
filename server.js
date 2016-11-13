0a747ee1a2af6a4b55530a0aa69a44a18efb7df6
Kevin Mark <kmark1625@gmail.com>
2016-11-13 17:38:29 -0600

    Now accepts paramter for timestamp

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.render('index');
+})
+
+app.get('/:timestamp', function(req, res) {
+  var timestamp = req.params.timestamp;
+  var parsedResponse = getParsedResponse(timestamp);
+  res.render('timestamp');
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+function getParsedResponse(timestamp) {
+    console.log('Hello World!');
+}
